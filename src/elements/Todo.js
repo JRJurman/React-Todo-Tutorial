4e87350ead18e108d181b3a1da9a5b9f10e4804d
jjurman <jjurman@constantcontact.com>
2018-11-04 23:11:46 -0500

    use this.state, remove animation

--- src/elements/Todo.js
+++ src/elements/Todo.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: props.isChecked || false
+    }
+  }
+
+  onCheck = () => {
+    this.setState({
+      checked: !this.state.checked
+    })
+  }
+
   render() {
     const elementStyle = {
-      textDecoration: this.props.isChecked ? 'line-through' : ''
+      textDecoration: this.state.checked ? 'line-through' : ''
     }
     return (
-      <div className="todo">
-        <input type="checkbox" checked={this.props.isChecked} />
+      <div className="todo" onClick={this.onCheck}>
+        <input type="checkbox" checked={this.state.checked} readOnly />
         <span style={elementStyle}>{this.props.children}</span>
       </div>
     );
